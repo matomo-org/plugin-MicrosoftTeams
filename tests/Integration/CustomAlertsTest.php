c763b980f2f7319a3d60dcd4e96d5e40bed3a591
Altamash Shaikh <altu9594@gmail.com>
2025-11-20 12:15:43 +0530

    Adds test for CustomAlerts via MSTeams, #PG-4671

---
+++ tests/Integration/CustomAlertsTest.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Matomo - free/libre analytics platform
+ *
+ * @link    https://matomo.org
+ * @license https://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+
+namespace Piwik\Plugins\MicrosoftTeams\tests;
+
+use Piwik\Plugins\CustomAlerts\CustomAlerts;
+use Piwik\Tests\Framework\TestCase\IntegrationTestCase;
+use Piwik\Tests\Framework\TestingEnvironmentManipulator;
+
+/**
+ * @group MicrosoftTeams
+ * @group CustomAlertsTest
+ * @group Plugins
+ */
+
+class CustomAlertsTest extends IntegrationTestCase
+{
+    public function setUp(): void
+    {
+        self::$fixture->extraPluginsToLoad = array('CustomAlerts');
+        TestingEnvironmentManipulator::$extraPluginsToLoad = self::$fixture->extraPluginsToLoad;
+
+        parent::setUp();
+
+        $pluginManager = \Piwik\Plugin\Manager::getInstance();
+        $pluginManager->loadPlugin('CustomAlerts');
+        $pluginManager->installLoadedPlugins();
+        $pluginManager->activatePlugin('CustomAlerts');
+    }
+    public function testGetReportMediumOptions()
+    {
+        $this->assertEquals([
+            ['key' => 'email', 'value' => 'CustomAlerts_MediumEmail', 'disabled' => false],
+            ['key' => 'mobile', 'value' => 'CustomAlerts_MediumMobile', 'disabled' => false],
+            ['key' => 'slack', 'value' => 'CustomAlerts_MediumSlack', 'disabled' => true],
+            ['key' => 'teams', 'value' => 'CustomAlerts_MediumMicrosoftTeams', 'disabled' => false],
+        ], CustomAlerts::getReportMediumOptions());
+    }
+}
